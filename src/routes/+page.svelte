9f91267d979368df513aa8ab92f657f197bae29d
george.nairooze <gnairooze@gmail.com>
2024-09-17 18:34:00 +0300

    add part1 tutorials from introduction to reactivity-assignments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Nested from "./Nested.svelte";
+
+    let name = `SvelteKit`;
+    let src = '/image.gif';
+
+    let string = `this string contains some <strong>HTML!!!</strong>`;
+
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
+
+<h1>Welcome to {name.toUpperCase()}</h1>
+
+<img {src} alt='{name} dances' />
+
+<p>This is a paragraph.</p>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Nested />
+
+<p>{@html string}</p>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
