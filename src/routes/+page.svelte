601589894f70072e9925b6ac41140fb0f57687ab
george.nairooze <gnairooze@gmail.com>
2024-09-19 14:53:33 +0300

    organize intro tutorials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,9 @@
 <script>
-    import Nested from "./Nested.svelte";
-
-    let name = `SvelteKit`;
-    let src = '/image.gif';
-
-    let string = `this string contains some <strong>HTML!!!</strong>`;
-
-	let count = 0;
-	$: doubled = count * 2;
-
-	$: {
-		console.log(`the count is ${count}`);
-		console.log(`this will also be logged whenever count changes`);
-	}
-
-	$: if (count >= 10) {
-		alert(`${count} is dangerously high!`);
-		count = 0;
-	}
-
-	function increment() {
-		count += 1;
-	}
+function redirect() {
+    window.location.replace("/intro/");
+}
 </script>
 
-<style>
-	p {
-		color: goldenrod;
-		font-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
-	}
-</style>
-
-<h1>Welcome to {name.toUpperCase()}</h1>
-
-<img {src} alt='{name} dances' />
-
-<p>This is a paragraph.</p>
-
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Nested />
-
-<p>{@html string}</p>
-
-<button on:click={increment}>
-	Clicked {count}
-	{count === 1 ? 'time' : 'times'}
+<button on:click={redirect}>
+    Go to /intro
 </button>
-
-<p>
-	{count} doubled is {doubled}
-</p>
