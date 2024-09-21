e662094b4bfd2fecdf88e0e1a2d55278f7afc5da
george.nairooze <gnairooze@gmail.com>
2024-09-21 11:08:42 +0300

    add if and else logic tutorials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,6 @@
     <ul>
         <li><a href="/intro-reactivity/">Go to intro-reactivity</a></li>
         <li><a href="/props/">Go to props</a></li>
+        <li><a href="/logic/">Go to logic</a></li>
     </ul>
 </nav>
