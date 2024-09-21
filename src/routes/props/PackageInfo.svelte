6674ccb3fbd79b0a158f4740ae467d20b0e5ba5f
george.nairooze <gnairooze@gmail.com>
2024-09-21 10:37:11 +0300

    add tutorial spread props

---
+++ src/routes/props/PackageInfo.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+	export let version;
+	export let speed;
+	export let website;
+
+	$: href = `https://www.npmjs.com/package/${name}`;
+</script>
+
+<p>
+	The <code>{name}</code> package is {speed} fast. Download version {version} from
+	<a {href} target="_blank">npm</a> and <a href={website} target="_blank">learn more here</a>
+</p>
