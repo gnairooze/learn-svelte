b54053452496fcb0c2e826683be11a292b0d800a
george.nairooze <gnairooze@gmail.com>
2024-09-24 12:03:15 +0300

    add dom events tutorial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,6 @@
         <li><a href="/intro-reactivity/">Go to intro-reactivity</a></li>
         <li><a href="/props/">Go to props</a></li>
         <li><a href="/logic/">Go to logic</a></li>
+        <li><a href="/events/">Go to events</a></li>
     </ul>
 </nav>
