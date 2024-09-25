ede6f4634eb7aa1241b81837ec65707af7b7cc4e
george.nairooze <gnairooze@gmail.com>
2024-09-25 20:35:51 +0300

    add numeric inputs tutorial

--- src/routes/bindings/+page.svelte
+++ src/routes/bindings/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	let name = 'world';
+	let a = 1;
+	let b = 2;
 </script>
 
 <h1>bindings</h1>
@@ -7,3 +9,19 @@
 <input bind:value={name} />
 
 <h2>Hello {name}!</h2>
+
+<div>
+	<label>
+		<input type="number" bind:value={a} min="0" max="10" />
+		<input type="range" bind:value={a} min="0" max="10" />
+	</label>
+</div>
+
+<div>
+	<label>
+		<input type="number" bind:value={b} min="0" max="10" />
+		<input type="range" bind:value={b} min="0" max="10" />
+	</label>
+</div>
+
+<p>{a} + {b} = {a + b}</p>
