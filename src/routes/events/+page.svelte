b57edd1ec68cd7e0fcb14ef4db9d8170604f69eb
george.nairooze <gnairooze@gmail.com>
2024-09-25 18:44:21 +0300

    add inline handlers tutorial

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,13 +1,20 @@
 <script>
 	let m = { x: 0, y: 0 };
 
-	function handleMove(event) {
+	/*function handleMove(event) {
 		m.x = event.clientX;
 		m.y = event.clientY;
-	}
+	}*/
 </script>
 
-<div on:pointermove={handleMove}>
+<!--<div on:pointermove={handleMove}>
+	The pointer is at {m.x} x {m.y}
+</div>-->
+
+<div on:pointermove={(e)=> {
+    m.x = e.clientX;
+    m.y = e.clientY;
+}}>
 	The pointer is at {m.x} x {m.y}
 </div>
 
