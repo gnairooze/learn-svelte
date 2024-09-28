587f03d15f2af771c69bcf21b479d22067a80238
george.nairooze <gnairooze@gmail.com>
2024-09-28 10:56:53 +0300

    add checkbox inputs tutorial

--- src/routes/bindings/+page.svelte
+++ src/routes/bindings/+page.svelte
@@ -2,6 +2,7 @@
 	let name = 'world';
 	let a = 1;
 	let b = 2;
+	let yes = false;
 </script>
 
 <h1>bindings</h1>
@@ -25,3 +26,24 @@
 </div>
 
 <p>{a} + {b} = {a + b}</p>
+
+<hr />
+
+<label>
+	<input type="checkbox" bind:checked={yes} />
+	Yes! Send me regular email spam
+</label>
+
+{#if yes}
+	<p>
+		Thank you. We will bombard your inbox and sell
+		your personal details.
+	</p>
+{:else}
+	<p>
+		You must opt in to continue. If you're not
+		paying, you're the product.
+	</p>
+{/if}
+
+<button disabled={!yes}>Subscribe</button>
